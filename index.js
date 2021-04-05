ab9e4a522f0e3a3d027fd40ff3f4c527c7c73073
Zach Frohardt <zfrohardt@gmail.com>
2021-04-05 14:20:29 -0700

    finished assignment with breakout group

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(wrapping = "*") {
+    return (string) => {
+        return `You are ${wrapping}${string}${wrapping}!`;
+    }
+}
